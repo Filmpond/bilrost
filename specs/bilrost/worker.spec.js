c4dc78f3ed1e4de5d01bfe09b2b8cd51491ffa04
Shirren Premaratne <shirren@me.com>
2016-11-03 22:08:59 +1100

    Started refactoring specs

--- specs/bilrost/worker.spec.js
+++ specs/bilrost/worker.spec.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var nconf = require('nconf');
-
-var expect  = require('chai').expect,
-    Worker = require('../../lib/bilrost/worker');
+var nconf = require('nconf'),
+  expect  = require('chai').expect,
+  Message = require('../../lib/bilrost/message'),
+  Worker = require('../../lib/bilrost/worker');
 
 describe('Worker', function() {
 
-  it('should be able to receive a message', function(done) {
-    var worker = new Worker('topic', 'subscriber');
-    worker.receive(done);
+  it('should be able to receive a valid message', function(done) {
+    var msg = new Message('some_data');
+    msg.postTo('topic', done);
+    var worker = new Worker('topic', 'subscriber', done);
+    worker.receive();
   });
 });
