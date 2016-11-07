e65cfd544c79d087c5d1cb068b886872294ef491
Shirren Premaratne <shirren@me.com>
2016-11-07 19:13:43 +1100

    Working through specs

--- specs/specs.common.js
+++ specs/specs.common.js
@@ -5,9 +5,6 @@ var winston = require('winston');
 module.exports = function() {
   try {
     winston.remove(winston.transports.Console);
-    winston.remove(winston.transports.File);
-    winston.remove(winston.transports.Http);
-    winston.remove(winston.transports.Memory);
     winston.add(winston.transports.File, { filename: 'test.log' });
   }
   catch (e) {
